8d4f5b8bb406310f398718e22e383c20ff066d4c
Thomas Camminady <24543638+camminady@users.noreply.github.com>
2021-08-30 17:53:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objs as go
 import pandas as pd
 
 app = dash.Dash()
-
+server = app.server
 df = pd.read_csv(
     'https://gist.githubusercontent.com/chriddyp/'
     'cb5392c35661370d95f300086accea51/raw/'
